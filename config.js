dbc0c0c9bcbc2a0b89fff5b152978c4830590fec
slhansa12 <115990333+slhansa12@users.noreply.github.com>
2025-05-19 21:20:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -46,7 +46,7 @@ module.exports = {
     ANTI_BAD: process.env.ANTI_BAD || "true",
     PREFIX: process.env.PREFIX || ".",
     FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
-    FAKE_TYPING: process.env.FAKE_TYPING || "true",
+    FAKE_TYPING: process.env.FAKE_TYPING || "false",
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
     CURRENT_STATUS: process.env.CURRENT_STATUS || "false",
     AUTO_REACT: process.env.AUTO_REACT || "false",
